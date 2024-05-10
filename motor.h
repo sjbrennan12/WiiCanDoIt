1659d1ef875ee8de89f7e8646776c03035240687
Thaddeus Serna <thaddeusserna@gmail.com>
2024-05-10 12:18:36 -0700

    Update motor.h

--- motor.h
+++ motor.h
@@ -1,5 +1,31 @@
 /***
 motor.h
-
-
+5-10-24
+motor.h file for WiiCanDoItProject
+Thaddeus Serna
 ***/
+
+class Motor{
+
+#ifndef MOTOR
+#define MOTOR
+
+public:
+	
+	long throttle;
+
+//constructor
+	Motor();
+
+//methods
+
+	void setThrottle(long input);
+
+	long getThrottle();
+
+	void emergencyBrake();	
+
+
+};
+
+#endif
