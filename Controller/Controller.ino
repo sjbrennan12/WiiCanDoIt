f0685520b676dc4ffe523d0862e72e031b81f32a
K-Reyna <91650651+K-Reyna@users.noreply.github.com>
2024-05-19 20:15:16 -0700

    Update Controller.ino
    added horn functionality and distance

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -14,13 +14,33 @@ VCC -> 5V
 RX -> TX
 TX -> RX
 
+Speaker
+GND -> GND
+DIGITAL -> 42
+
+HC-SR04
+VCC -> 5V
+TRIG ->
+ECHO ->
+GND -> GND
 */
+
+#include "SR04.h"
+
+#define SPEAKER_PIN 42
+#define TRIG_PIN 49
+#define ECHO_PIN 51
+
+SR04 distanceSensor = SR04(ECHO_PIN,TRIG_PIN);
 
 int input;
 int reverse;
 int turn;
 int horn;
 int brake;
+long distance;
+char start;
+
 
 void setup() {
   Serial.begin(38400); 
@@ -28,12 +48,13 @@ void setup() {
 
 void loop() {
   if(Serial.available() > 0) { 
-    
+
     input = Serial.read() * 10;
     reverse = Serial.read();
     turn = Serial.read();
     horn = Serial.read();
     brake = Serial.read();
+
 
     Serial.print("Speed: ");
     Serial.println(input);
@@ -46,10 +67,21 @@ void loop() {
     
     Serial.print("Horn: ");
     Serial.println(horn);
-    
+
     Serial.print("Brake: ");
     Serial.println(brake);
+    
+    distance = distanceSensor.Distance();
+    Serial.print("Distance CM: ");
+    Serial.println(distance);
+    
+
+    if (horn == 1) {
+      tone(SPEAKER_PIN, 450, 300);
+      horn = 0;
+    }
+
   }
   
-  delay(250);
+  delay(100);
 }
